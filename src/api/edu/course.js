5c28152640fba282f36ae5ad3f9c859d9432f0a1
lidian <837997288@qq.com>
2022-10-28 16:44:33 +0800

    init2

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -14,5 +14,45 @@ export default {
             url: '/eduservice/teacher/findAll',
             method: 'get'
           })
-    }
+    },
+  //根据课程id查询课程基本信息
+  getCourseInfoId(id) {
+    return request({
+      url: '/eduservice/course/getCourseInfo/'+id,
+      method: 'get'
+    })
+  },
+  //修改课程信息
+  updateCourseInfo(courseInfo) {
+    return request({
+      url: '/eduservice/course/updateCourseInfo',
+      method: 'post',
+      data: courseInfo
+    })
+  },
+  //==================================================================
+
+  //课程确认信息显示
+  getPublihCourseInfo(id) {
+    return request({
+      url: '/eduservice/course/getPublishCourseInfo/'+id,
+      method: 'get'
+    })
+  },
+  //课程最终发布
+  publihCourse(id) {
+    return request({
+      url: '/eduservice/course/publishCourse/'+id,
+      method: 'post'
+    })
+  },
+  //TODO 课程列表
+  //课程最终发布
+  getListCourse() {
+    return request({
+      url: '/eduservice/course',
+      method: 'get'
+    })
+  },
+
 }
